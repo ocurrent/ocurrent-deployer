dbc4e92adbf77405f51b718eda12073971e5bb40
Thomas Leonard <talex5@gmail.com>
2020-02-17 11:45:27 +0000

    Remove some trailing whitespace

--- src/pipeline.ml
+++ src/pipeline.ml
@@ -3,4 +3,4 @@ module Git = Current_git
 let v ~repo () =
   (* Replace this with your actual pipeline: *)
   let src = Git.Local.head_commit repo in
-  Current.ignore_value src      
+  Current.ignore_value src
