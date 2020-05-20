cce9f2cf9ddbbff18af68ae0faadc5e4e71fc859
Thomas Leonard <talex5@gmail.com>
2020-05-20 17:05:17 +0100

    Refactor deployer
    Instead of trying to make the build code work with both Docker services
    and unikernels, provide a functor that can be used with whatever types
    you like.

--- src/build.ml
+++ src/build.ml
@@ -14,55 +14,72 @@ let org ~app ~account id =
 
 let head_of ~github repo name = Github.Api.head_of github repo (`Ref ("refs/heads/" ^ name))
 
-(* Strip deployment information and remove duplicates. *)
-let ignore_branch items =
-  items
-  |> List.map (function
-      | (d, _, `Docker _) -> d, `Docker
-      | (d, _, `Unikernel (_, flags)) -> d, `Unikernel flags
-    )
-  |> List.sort_uniq compare
-
-let status_of_build ~url b =
-  let+ state = Current.state b in
-  match state with
-  | Ok _              -> Github.Api.Status.v ~url `Success ~description:"Passed"
-  | Error (`Active _) -> Github.Api.Status.v ~url `Pending
-  | Error (`Msg m)    -> Github.Api.Status.v ~url `Failure ~description:m
-
-let repo ~web_ui ~build ~deploy ~org:(org, github) ~name builds =
-  let repo_name = Printf.sprintf "%s/%s" org name in
-  let repo = { Github.Repo_id.owner = org; name } in
-  let root = Current.return ~label:repo_name () in      (* Group by repo in the diagram *)
-  Current.with_context root @@ fun () ->
-  let builds =
-    let refs = Github.Api.ci_refs github repo in
-    let collapse_value = repo_name ^ "-builds" in
-    let url = web_ui collapse_value in
-    let pipeline =
-      refs
-      |> Current.list_iter (module Github.Api.Commit) @@ fun commit ->
-      let src = Git.fetch (Current.map Github.Api.Commit.id commit) in
-      Current.all (
-        ignore_branch builds |> List.map (fun (dockerfile, target) -> build ~dockerfile ~src target)
-      )
-      |> status_of_build ~url
-      |> Github.Api.Commit.set_status commit "deployability"
-    in
-    Current.collapse
-      ~key:"repo" ~value:collapse_value
-      ~input:refs pipeline
-  and deployments =
-    Current.all (
-      builds |> List.map (fun (dockerfile, branch, target) ->
-          let commit = head_of ~github repo branch in
-          let src = Git.fetch (Current.map Github.Api.Commit.id commit) in
-          let collapse_value = Printf.sprintf "%s-%s-%s" repo_name dockerfile branch in
-          let build = deploy ~dockerfile ~src ~commit ~collapse_value target in
-          Current.collapse
-            ~key:"repo" ~value:collapse_value
-            ~input:commit build
-        )
-    )
+(* Push a Slack notification to [channel] to say that [x] is updating [service] to [commit].
+   [repo] is used for the URL in the message. *)
+let notify ~channel ~web_ui ~service ~commit ~repo x =
+  let s =
+    let+ state = Current.state x
+    and+ commit = commit in
+    let uri = Github.Api.Commit.uri commit in
+    Fmt.strf "@[<h>Deploy <%a|%a> as %s: <%s|%a>@]"
+      Uri.pp uri Github.Api.Commit.pp commit
+      service
+      (Uri.to_string (web_ui repo)) (Current_term.Output.pp Current.Unit.pp) state
   in
-  Current.all [builds; deployments]
+  Current.all [
+    Current_slack.post channel ~key:("deploy-" ^ service) s;
+    x   (* If [x] fails, the whole pipeline should fail too. *)
+  ]
+
+module Make(T : S.T) = struct
+  let status_of_build ~url b =
+    let+ state = Current.state b in
+    match state with
+    | Ok _              -> Github.Api.Status.v ~url `Success ~description:"Passed"
+    | Error (`Active _) -> Github.Api.Status.v ~url `Pending
+    | Error (`Msg m)    -> Github.Api.Status.v ~url `Failure ~description:m
+
+  let repo ~channel ~web_ui ~org:(org, github) ~name build_specs =
+    let repo_name = Printf.sprintf "%s/%s" org name in
+    let repo = { Github.Repo_id.owner = org; name } in
+    let root = Current.return ~label:repo_name () in      (* Group by repo in the diagram *)
+    Current.with_context root @@ fun () ->
+    let builds =
+      let refs = Github.Api.ci_refs github repo in
+      let collapse_value = repo_name ^ "-builds" in
+      let url = web_ui collapse_value in
+      let pipeline =
+        refs
+        |> Current.list_iter (module Github.Api.Commit) @@ fun commit ->
+        let src = Git.fetch (Current.map Github.Api.Commit.id commit) in
+        Current.all (
+          build_specs |> List.map (fun (build_info, _deploys) -> T.build build_info src |> Current.ignore_value)
+        )
+        |> status_of_build ~url
+        |> Github.Api.Commit.set_status commit "deployability"
+      in
+      Current.collapse
+        ~key:"repo" ~value:collapse_value
+        ~input:refs pipeline
+    and deployments =
+      Current.all (
+        build_specs |> List.map (fun (build_info, deploys) ->
+            Current.all (
+              deploys |> List.map (fun (branch, deploy_info) ->
+                  let service = T.name deploy_info in
+                  let collapse_value = Printf.sprintf "%s-%s-%s" repo_name service branch in
+                  let commit = head_of ~github repo branch in
+                  let src = Git.fetch (Current.map Github.Api.Commit.id commit) in
+                  let binary = T.build build_info src in
+                  T.deploy deploy_info binary
+                  |> notify ~channel ~web_ui ~service ~commit ~repo:collapse_value
+                  |> Current.collapse
+                    ~key:"repo" ~value:collapse_value
+                    ~input:commit
+                )
+            )
+          )
+      )
+    in
+    Current.all [builds; deployments]
+end
