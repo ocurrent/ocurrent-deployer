b12d2b9748a8137bc563c1e8c62c31006f0f01c3
Mark Elvers <mark.elvers@tunbury.org>
2022-05-17 16:45:10 +0000

    Rename 'opam3-ocaml-org' Docker context

--- src/pipeline.ml
+++ src/pipeline.ml
@@ -116,7 +116,7 @@ module Cluster = struct
   module Ocamlorg_docker = Current_docker.Make(struct let docker_context = Some "ocaml-www1" end)
   module V3ocamlorg_docker = Current_docker.Make(struct let docker_context = Some "v3-ocaml-org" end)
 
-  module Opamocamlorg_docker = Current_docker.Make(struct let docker_context = Some "opam3-ocaml-org" end)
+  module Opamocamlorg_docker = Current_docker.Make(struct let docker_context = Some "opam-3.ocaml.org" end)
   module V2ocamlorg_docker = Current_docker.Make(struct let docker_context = Some "v2-ocaml-org" end)
   module Ocamlorg_images = Current_docker.Make(struct let docker_context = Some "ci3.ocamllabs.io" end)
   module Deploycamlorg_docker = Current_docker.Default
