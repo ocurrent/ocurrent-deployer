c1d4fe49787b66e7063a825b62bf2bd4575092db
Shon Feder <shon.feder@gmail.com>
2024-09-16 23:07:26 -0400

    Simplify CLI logic
    Now that we have the constellation absraction, it is clear how we can
    reduce code duplication in deriving the engine for a flavor.
    
    Also there the `flavour_opt` type was redundnat, because, any type
    
    ```
    type t =
      | C1 of t'
      | C2 of t'
      | ...
      | Cn of t'
    ```
    
    is isomorphic to
    
    ```
    type c = C1 | C2 | ... | Cn
    type t = C * t'
    ```
    
    And we already have the sum `Pipeline.Flavor.t` for our `c`.

--- src/main.ml
+++ src/main.ml
@@ -2,11 +2,6 @@
 
 open Deployer
 
-type flavour_opt =
-  | Tarides of Uri.t
-  | OCaml of Uri.t
-  | Mirage of Uri.t
-
 (* A low-security Docker Hub user used to push images to the staging area.
    Low-security because we never rely on the tags in this repository, just the hashes. *)
 let staging_user = "ocurrentbuilder"
@@ -23,26 +18,19 @@ let read_channel_uri path =
   with ex ->
     Fmt.failwith "Failed to read slack URI from %S: %a" path Fmt.exn ex
 
-let main () config mode app slack auth staging_password_file flavour prometheus_config =
+let main () config mode app slack auth staging_password_file (flavour, sched) prometheus_config =
   let vat = Capnp_rpc_unix.client_only_vat () in
   let channel = read_channel_uri slack in
   let staging_auth = staging_password_file |> Option.map (fun path -> staging_user, read_first_line path) in
-  let engine, admins = match flavour with
-    | Tarides sched ->
-       let sched = Current_ocluster.Connection.create (Capnp_rpc_unix.Vat.import_exn vat sched) in
-       Current.Engine.create ~config (Pipeline.Tarides.v ~app ~notify:channel ~sched ~staging_auth),
-       Pipeline.Tarides.admins
-    | OCaml sched ->
-       let sched = Current_ocluster.Connection.create (Capnp_rpc_unix.Vat.import_exn vat sched) in
-       Current.Engine.create ~config (Pipeline.Ocaml_org.v ~app ~notify:channel ~sched ~staging_auth),
-       Pipeline.Ocaml_org.admins
-    | Mirage sched ->
-       let sched = Current_ocluster.Connection.create (Capnp_rpc_unix.Vat.import_exn vat sched) in
-       Current.Engine.create ~config (Pipeline.Mirage.v ~app ~notify:channel ~sched ~staging_auth),
-       Pipeline.Mirage.admins
-  in
   let authn = Option.map Current_github.Auth.make_login_uri auth in
   let webhook_secret = Current_github.App.webhook_secret app in
+  let sched = Current_ocluster.Connection.create (Capnp_rpc_unix.Vat.import_exn vat sched) in
+  let pipeline, admins = match flavour with
+    | `Tarides -> Pipeline.Tarides.(v, admins)
+    | `OCaml -> Pipeline.Ocaml_org.(v, admins)
+    | `Mirage -> Pipeline.Mirage.(v, admins)
+  in
+  let engine = Current.Engine.create ~config (pipeline ~app ~notify:channel ~sched ~staging_auth) in
   let has_role =
     if auth = None then
       Current_web.Site.allow_all
@@ -84,15 +72,12 @@ let submission_service =
     ~docv:"FILE"
     ["submission-service"]
 
-let flavour_opt =
-  let f s fl : flavour_opt Term.ret = match s,fl with
-    | Some s, `OCaml -> `Ok (OCaml s)
-    | Some s, `Tarides -> `Ok (Tarides s)
-    | Some s, `Mirage -> `Ok (Mirage s)
-    | None, `Mirage -> `Error (true, "--submission-service required for --flavour mirage")
-    | None, `OCaml -> `Error (true, "--submission-service required for --flavour ocaml")
-    | None, `Tarides -> `Error (true, "--submission-service required for --flavour tarides") in
-
+let flavour_and_sub_service =
+  let f sub_service flavor : (Pipeline.Flavour.t * Uri.t) Term.ret = match sub_service with
+    | Some s -> `Ok (flavor, s)
+    | None ->
+      `Error (true, "--submission-service required for --flavour " ^ (Pipeline.Flavour.to_string flavor))
+  in
   Term.(ret (const f $ submission_service $ Pipeline.Flavour.cmdliner))
 
 let staging_password =
@@ -106,7 +91,7 @@ let staging_password =
 let cmd =
   let doc = "build and deploy services from Git" in
   let cmd_t = Term.(term_result (const main $ Logging.cmdliner $ Current.Config.cmdliner $ Current_web.cmdliner $
-        Current_github.App.cmdliner $ slack $ Current_github.Auth.cmdliner $ staging_password $ flavour_opt
+        Current_github.App.cmdliner $ slack $ Current_github.Auth.cmdliner $ staging_password $ flavour_and_sub_service
         $ Prometheus_unix.opts)) in
   let info = Cmd.info "deploy" ~doc in
   Cmd.v info cmd_t
