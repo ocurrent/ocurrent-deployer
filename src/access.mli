b63e098890c9a0e4c4e96ff792e440975ad1958c
Shon Feder <shon.feder@gmail.com>
2024-09-16 21:42:13 -0400

    Create helper for access control logic
    This will let us remove duplication from the access control logic.
    It also adds tests for this logic, and uses (imo) a more readable
    implementation of the logic.
    
    We also add tests to verify it works as expected. This is very simple
    logic but it is critical for secrurity, thus my motive for fussing with
    it.

---
+++ src/access.mli
@@ -0,0 +1,1 @@
+val user_has_role : admins:string list -> string option -> Current_web.Role.t -> bool
--- test/test_index.ml
+++ test/test_index.ml
@@ -12,7 +12,25 @@ let test_simple () =
   Index.record ~repo ~hash [ ("build", Some "job2") ];
   Alcotest.(check (list string)) "Job-ids" ["job2"] @@ List.sort String.compare @@ Index.get_job_ids ~owner ~name ~hash
 
+let test_access_role () =
+  let case msg = Alcotest.(check' bool) ~msg in
+  let admins = ["Kenta"; "Hai"; "Jakob"] in
+  let has_role = Deployer.Access.user_has_role ~admins in
+  case "unauthenticated users SHOULD have view and monitor role"
+    ~expected:true
+    ~actual:(List.for_all (has_role None) [`Viewer; `Monitor]);
+  case "unauthenticated users MUST NOT have admin or builder role"
+    ~expected:false
+    ~actual:(List.for_all (has_role None) [`Admin; `Builder]);
+  case "non-admin users MUST NOT have admin or builder role"
+    ~expected:false
+    ~actual:(has_role (Some "Auden") `Admin || has_role (Some "Wenjing") `Builder);
+  case "admin users SHOULD have every role"
+    ~expected:true
+    ~actual:(List.for_all (has_role (Some "Kenta")) [`Viewer; `Monitor; `Builder; `Admin])
+
+
 let tests = [
     Alcotest_lwt.test_case_sync "simple" `Quick test_simple;
+    Alcotest_lwt.test_case_sync "access roles" `Quick test_access_role;
 ]
-
