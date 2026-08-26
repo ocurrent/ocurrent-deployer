210e2d7e22e3d2501076d52dc7f102aac7017a17
Mark Elvers <mark.elvers@tunbury.org>
2026-08-26 14:06:04 +0000

    Repoint mirage-www deploys from Equinix infra-2 to dopey
    infra-2 (147.75.84.37) was decommissioned with the Equinix Metal
    shutdown; the MirageOS unikernels now run on dopey.caelum.ci.dev
    in Cambridge. mirage-redeploy on dopey handles the www and next
    services with their new addresses.

--- src/packet_unikernel.ml
+++ src/packet_unikernel.ml
@@ -1,4 +1,4 @@
-(** Mirage unikernels built via OCluster and deployed to packet.net *)
+(** Mirage unikernels built via OCluster and deployed to dopey.caelum.ci.dev *)
 
 open Current.Syntax
 
@@ -42,7 +42,7 @@ let name { service } = service
 
 (* Deployment *)
 
-let mirage_host_ssh = "root@147.75.84.37"
+let mirage_host_ssh = "root@dopey.caelum.ci.dev"
 
 let deploy build_info { service } ?additional_build_args:_ src =
   let repo_id = build_and_push build_info src in
